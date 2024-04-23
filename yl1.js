48ae47131b371ab081a07c32a24fe72e19061d7b
daskoder <150146748+daskoder@users.noreply.github.com>
2024-04-23 10:55:49 +0300

    Add files via upload

---
+++ yl1.js
@@ -0,0 +1,19 @@
+/* Kennert Heiden
+23.04
+Yl1*/
+let arv = 4.2
+console.log("Number =", typeof arv);
+let alerto = 'Will Code 4 Food'
+console.log("String:", typeof alerto);
+let stringy = "123"
+console.log("string:",typeof stringy);
+let bolen = true
+console.log("Boolean", typeof bolen);
+let arr = [1,2,3]
+console.log("Array:", typeof arr);
+/*let bolan2 = FALSE
+console.log("Did you have a treat?", typeof bolan2);*/
+let bolan3 = 'true'
+console.log("String:", typeof bolan3);
+let nothing = undefined
+console.log("Value:", typeof nothing);
